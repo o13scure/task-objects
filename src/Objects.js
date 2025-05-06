a659640128a1eb85d4f4d7de53b80c7035c4c2e6
Alexandr Ukraintsev <sadman1337322@gmail.com>
2025-05-06 21:43:32 +0300

    задание на объекты

--- src/Objects.js
+++ src/Objects.js
@@ -8,6 +8,12 @@
   Объект после манипуляций следует вернуть в качестве результата работы функции.
 */
 export function personUpdate(data) {
+      if (data.gender === 'female') {
+        delete data.age;
+    } else if (data.gender === 'male' && !data.income) {
+        data.income = 100000;
+    }
+    return data;
 }
 
 /*
@@ -15,6 +21,8 @@ export function personUpdate(data) {
   Верните список названий этих полей в алфавитном порядке в виде массива строк.
 */
 export function objectFieldsList(obj1, obj2, obj3) {
+  const keys = new Set([...Object.keys(obj1), ...Object.keys(obj2), ...Object.keys(obj3)]);
+  return Array.from(keys).sort();
 }
 
 /*
@@ -23,4 +31,8 @@ export function objectFieldsList(obj1, obj2, obj3) {
   Количество клонов - count.
 */
 export function objectClone(obj, count) {
+  return Array.from({ length: count }, (v, i) => ({
+    ...obj,
+    id: i + 1
+}));
 }
